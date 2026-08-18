f456e8439361c30d67e99d5773891014245bf85d
Anantharajts <anantharajtsananthu@gmail.com>
2026-08-18 11:38:31 +0530

    oooo

---
+++ login.php
@@ -0,0 +1,20 @@
+
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/CSS/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="assets/CSS/login.css" type="text/css">
+    <script src="assets/JS/jquery.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    
+</body>
+</html>
